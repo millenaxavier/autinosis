05a96596037ee60f7a2e9d75aa96324e27fcc2e1
Millena Xavier Martins <104324790+millenaxavier@users.noreply.github.com>
2023-06-20 13:48:21 -0300

    Update firstautie.py

--- firstautie.py
+++ firstautie.py
@@ -1,8 +1,22 @@
-git clone https://github.com/millenaxavier/autinosis.git
-
-import pandas as pd
-from sklearn.externals import joblib
-import streamlit as st
-
 # Restante do seu código Streamlit...
 streamlit run app.py
+# Inicie um repositório Git no diretório do seu projeto
+git init
+# app.py
+print(app.py)
+# Adicione o arquivo ao controle de versão do Git
+git add app.py
+# Confirme as alterações no repositório Git
+git commit -m "Iniciar o projeto"
+# Instale o Streamlit usando o pip
+pip install streamlit
+# streamlit_app.py
+import streamlit as st
+
+def main():
+    st.write("Olá, mundo no Streamlit!")
+
+if __name__ == "__main__":
+    main()
+# Execute o aplicativo Streamlit
+streamlit run streamlit_app.py
